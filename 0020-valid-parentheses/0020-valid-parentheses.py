e673eb44db8631e1207c9851e569e458ca008200
jed212 <jedidahfavour@gmail.com>
2023-09-30 14:51:09 +0300

    Time: 37 ms (68.95%), Space: 16.2 MB (74.08%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,24 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        match_dict = {'(':')', '{':'}', '[':']'}
+        match_dict = {')':'(', '}':'{', ']':'['}
         
         for c in s:
-            if c == '(' or c == '[' or c == '{':
+            if c not in match_dict:
                 stack.append(c)
-            elif not stack:
+                continue
+            if not stack or match_dict[c] != stack[-1]:
                 return False
-            elif stack and c == ')' or c == ']' or c == '}':
-                last = stack.pop()
-                
-                if last in match_dict and match_dict[last] == c:
-                    continue
-                else:
-                    return False
-            else:
-                return False
-        if not stack:
-            return True
-        else:
-            return False
-                    
+            stack.pop()
+            
+        return not stack
